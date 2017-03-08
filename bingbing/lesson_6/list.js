7d458465fde1cf0f43323b6d6be626280280c81e
Away0x <wutong0910@foxmail.com>
2017-03-08 11:28:52 +0800

    'bingbing_lesson6'

--- bingbing/lesson_6/list.js
+++ bingbing/lesson_6/list.js
@@ -4,26 +4,13 @@ class List {
     this._$wrap     = $(config.wrapSelector) // 队列容器
     this._arr       = []                     // 所有行的文字内容
     this._domLi     = []                     // 所有行 dom
-    this._domB      = []                     // 所有 b dom
-  }
-  // 生成 b : Number -> return dom(html b)
-  _renderB (str) {
-    return createElm('b', str)
   }
   // 生成 li : Number -> return dom(html li)
   _renderLi (str, index=0) {
-    const
-      $li = createElm('li'),
-      b_domArr = []
+    const $li = createElm('li', str)
 
     $li.setAttribute('active-count', 0)
     $li.setAttribute('title', '匹配数 0')
-    str.split('')
-      .map(s => this._renderB(s))
-      .map($b => b_domArr.push($b)   && $b)
-      .map($b => this._domB.push($b) && $b)
-      .forEach($b => $li.appendChild($b))
-
     this._domLi.push($li)
     return $li
   }
@@ -31,7 +18,6 @@ class List {
   _renderList () {
     this._$wrap.innerHTML = ''
     this._domLi = []
-    this._domB = []
     this._arr
       .map(s => String(s))
       .map(s => s.trim())
@@ -41,23 +27,31 @@ class List {
   }
   // 匹配内容多的 li 位置会提升
   _sorList () {
-    const intAttr = $el => parseInt( $el.getAttribute('active-count') )
+    const
+      intAttr = $el => parseInt( $el.getAttribute('active-count') )
+    let matchListCount = 0
 
     this._$wrap.innerHTML = ''
     this._domLi
       .map($li => {
-          const activeBCount = [].filter.call($li.children, $b => $b.className === 'active').length
+          const activeBCount = $li.children.length
 
           $li.setAttribute('active-count', activeBCount)
           $li.setAttribute('title', `匹配数 ${activeBCount}`)
+          if (activeBCount) matchListCount += 1
           return $li
       })
       .sort((a, b) => intAttr(b) - intAttr(a))
       .forEach($li => this._$wrap.appendChild($li))
+
+    if (matchListCount === 0)
+      alert('没有匹配到的文本')
   }
-  //
+  // 匹配文本
   eachAllBTextEqualInput (str) {
-    this._domB.forEach($b => $b.className = ($b.textContent === str) ? 'active' : '')
+    this._domLi.forEach($li => {
+      $li.innerHTML = $li.innerHTML.replace(new RegExp(str, 'g'), `<b>${str}</b>`)
+    })
     this._sorList()
   }
   push (str) {
